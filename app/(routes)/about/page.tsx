6d1f9405ef42cd860762f819ea418c2da17a5f5e
salubBhai <salub7291@gmail.com>
2024-12-06 21:48:03 +0530

    feat:added about page

---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,11 @@
+import About from "@/components/app/About";
+
+const ContactPage = () => {
+    return (
+        <div className="mt-10">
+            <About />
+        </div>
+    )
+};
+
+export default ContactPage;
